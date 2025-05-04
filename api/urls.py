4b63260d302adab7cce580953553d8de82afeb4c
Manolo Valdés <39590957+Manolo-Valdes@users.noreply.github.com>
2025-05-04 16:51:46 +0200

    use an APIView

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import hello_world, MyView
+from .views import MyView
 
 urlpatterns = [
-    path('hello/', hello_world),
     path('message/', MyView.as_view(), name='my_view'),
 ]
--- api/views.py
+++ api/views.py
@@ -2,16 +2,9 @@ import inject
 
 # Create your views here.
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 
 from domain.contracts.Basic import IGreetings
-
-
-@api_view(['GET'])
-def hello_world(request):
-    greetings: IGreetings = inject.instance(IGreetings)
-    return Response({"message": greetings.hello('world')})
 
 
 class MyView(APIView):
